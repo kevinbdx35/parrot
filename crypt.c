039e4f583d5d98c9760c7523013002c7545357cf
Kevin <k.bardoux@orange.fr>
2023-08-28 23:27:39 +0200

    update the mask operation

--- crypt.c
+++ crypt.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <time.h>
 
+
 void crypt(void)
 {
 
@@ -11,7 +12,22 @@ void crypt(void)
     char *phrase= "HI!DD";
     char *parrot="toto";
 
+    char *mask = NULL;
+
+
+
+    int length = 0;
+
     printf("%s %s\n", phrase, parrot);
+
+    length = abs(strlen(phrase) - strlen(parrot));
+    printf("%i\n", length);
+
+    char buffer[length];
+    strncat(strcpy(buffer, parrot), parrot, length);
+
+    printf("%s\n", buffer);
+
 
     for(int i = 0, m = strlen(phrase); i < m ; i++)
     {
@@ -19,21 +35,15 @@ void crypt(void)
         char *x = NULL;
         x = phrase[i];
         char *y = NULL;
-        y = parrot[i];
-        char *mask = NULL;
+        y = buffer[i];
+
 
         mask = abs(x - y);
 
         swappy(&x, &y);
 
-        printf("%c\n", x);
-        printf("%i\n", mask);
-
-    }
-
-
-
-
-
+        printf("%c", x);
+        printf("\nle mask est %i", mask);
+        }
 
 }
