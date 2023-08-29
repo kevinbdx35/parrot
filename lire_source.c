4d1ab1e841f9efa2e562f4228ca91acec95f3d09
Kevin <k.bardoux@orange.fr>
2023-08-29 10:21:52 +0200

    update lire perroquet and add lire source

--- lire_source.c
+++ lire_source.c
@@ -6,13 +6,13 @@
 
 #define TAILLE_MAX 1000
 
-char *lire_perroquet(void)
+char *lire_source(void)
 {
 
-    char chaine_perroquet[TAILLE_MAX]="";
+    char chaine_source[TAILLE_MAX]="";
 
     // OPEN TXT FILE
-    FILE *file = fopen("peroq.def", "r");
+    FILE *file = fopen("source.txt", "r");
 
     if (!file)
     {
@@ -22,13 +22,13 @@ char *lire_perroquet(void)
 
 
     // READ DATA OF FILE
-    fgets(chaine_perroquet, TAILLE_MAX, file);
+    fgets(chaine_source, TAILLE_MAX, file);
 
     // PRINT THESE DATA
-    printf("%s", chaine_perroquet);
+    printf("%s", chaine_source);
 
     // CLOSE FILE
     fclose(file);
 
-    return chaine_perroquet;
+    return chaine_source;
 }
