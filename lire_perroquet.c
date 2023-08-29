6e87ba6c2b4af1c5e82659565aa3d0ba10662fe2
Kevin <k.bardoux@orange.fr>
2023-08-29 10:08:07 +0200

    update lire le perroquet

--- lire_perroquet.c
+++ lire_perroquet.c
@@ -4,11 +4,32 @@
 #include <string.h>
 #include <time.h>
 
+#define TAILLE_MAX 1000
+
 void lire_perroquet(void)
 {
 
+    char chaine[TAILLE_MAX]="";
+
+    // OPEN TXT FILE
+    FILE *file = fopen("peroq.def", "r");
+
+    if (!file)
+    {
+        printf("Erreur lors de l'ouverture d'un fichier");
+        exit(1);
+    }
 
 
+    // READ DATA OF FILE
+    fgets(chaine, TAILLE_MAX, file);
 
+    // PRINT THESE DATA
+    printf("%s", chaine);
+
+    // CLOSE FILE
+    fclose(file);
+
+    return 0;
 
 }
